5df7911d55bf82efe166d71f63d5bc4ce9fa096d
np <nandanpatilm15@gmail.com>
2025-01-11 21:40:05 +0530

    Changed the camera resolution to 4:3

--- script.js
+++ script.js
@@ -26,8 +26,8 @@ async function initCamera() {
         const constraints = {
             video: {
                 deviceId: backCamera ? { exact: backCamera.deviceId } : undefined,
-                width: { ideal: 3840 }, // 4K
-                height: { ideal: 2160 }, // 4K
+                width: 4096, // 4K
+                height: 3072, // 4K
                 facingMode: backCamera ? undefined : "environment",
             },
         };
