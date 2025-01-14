90dcb77f13fad80753d50ddf53e720ea149def5e
np <nandanpatilm15@gmail.com>
2025-01-14 18:46:44 +0530

    Updated some parts of the code

--- script.js
+++ script.js
@@ -26,8 +26,8 @@ async function initCamera() {
         const constraints = {
             video: {
                 deviceId: backCamera ? { exact: backCamera.deviceId } : undefined,
-                width: {ideal: 3072}, // 4K
-                height: {ideal: 4096}, // 4K
+                width: 3072, // 4K
+                height: 4096, // 4K
                 facingMode: backCamera ? undefined : "environment",
             },
         };
